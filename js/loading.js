98b213d958e2c7aecb3d7fb185c6105186f5354f
Thenujan Sandramohan <sthenujan2002@gmail.com>
2022-06-06 15:46:36 +0530

    add mistakenly deleted loading animation

--- js/loading.js
+++ js/loading.js
@@ -1,30 +1,3 @@
 
-// let jumper=(element)=>{
-//     let miniJumper=()=>{
-//         console.log(element)
-//         let elem =$(element)
-//         if (elem.hasClass("ground")){
-//             elem.addClass("jumping")
-//             elem.removeClass("ground")
-//         }else{
-//             elem.addClass("ground")
-//             elem.removeClass("jumping")
-//         }
-//     }
-//     miniJumper();
-//     setTimeout(miniJumper,500)
-// }
-
-
-// $(document).ready(()=>{
-//     console.log($("loading-wrapper"))
-//     $(".fa-circle").each((index,value)=>{
-//         console.log(value)
-//         setTimeout(()=>{setInterval(jumper,3000,[value])},(index)*1000)
-
-//     })
-// })
-
-
 
 
